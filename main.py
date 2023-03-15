13921118820784b64628a945fd128292a8ec9362
turko2373 <turko1073@gmail.com>
2023-03-14 23:55:39 -0300

    realice algunos arreglos de presentacion

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ app.include_router(marcas.router)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
+
 if __name__ == "__main__":    
     uvicorn.run("main:app", port = 8000, reload = True)
